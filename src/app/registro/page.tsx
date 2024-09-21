5896787337bcc1913a16ffe26b331d73ecc4da97
yeriic <piratebay.swagga10@gmail.com>
2024-09-21 15:52:18 -0500

    Modificación a la distribucion de los elemento en registro

--- src/app/registro/page.tsx
+++ src/app/registro/page.tsx
@@ -8,32 +8,41 @@ const registro: React.FC = () => {
             <img src="foto_sesion/logo.png" alt="Logo de la página"/> 
             <h2>Registro</h2>
             <form action="validacion_registro.jsp" method="post">
-            
-            	<div className="inputbox">
-                    <label htmlFor="usuario">Nombre de usuario</label>
-                    <input type="text" id="usuario" name="usuario" placeholder="Ingrese su nombre de usuario" required/>
+                <div className='division'>
+                <div className='div_uno'>          
+                    <div className="inputbox">
+                        <label htmlFor="usuario">Nombre de usuario</label>
+                        <input type="text" id="usuario" name="usuario" placeholder="Ingrese su nombre de usuario" required/>
+                    </div>
+                    
+                    <div className="inputbox">
+                        <label htmlFor="nombre" >Nombre</label>
+                        <input type="text" id="nombre" name="nombre" placeholder="Ingrese su nombre" required/>
+                    </div>
+                    
+                    <div className="inputbox">
+                        <label htmlFor="apellido" >Apellido</label>
+                        <input type="text" id="apellido" name="apellido" placeholder="Ingrese su apellido" required/>
+                    </div>
+
+                </div>  
+                <div className='div_dos'>
+                    <div className="inputbox">
+                        <label htmlFor="email">Correo electrónico</label>
+                        <input type="email" id="email" name="email" placeholder="Ingrese su correo electronico" required/>
+                    </div>
+
+                    <div className="inputbox">
+                        <label htmlFor="contrasena">Contraseña</label>
+                        <input type="password" id="contrasena" name="contrasena" placeholder="Ingrese su contraseña " required/>
+                    </div>
+
+                        <div className="inputbox">
+                        <label htmlFor="contrasena_confirmar">Confrimar contraseña</label>
+                        <input type="password" id="contrasena_confirmar" name="contrasena_confirmar" placeholder="Ingrese su contraseña " required/>
+                    </div>
                 </div>
-                
-                <div className="inputbox">
-                    <label htmlFor="nombre" >Nombre</label>
-                    <input type="text" id="nombre" name="nombre" placeholder="Ingrese su nombre" required/>
                 </div>
-                
-                  <div className="inputbox">
-                    <label htmlFor="apellido" >Apellido</label>
-                    <input type="text" id="apellido" name="apellido" placeholder="Ingrese su apellido" required/>
-               	  </div>
-
-               	 <div className="inputbox">
-                    <label htmlFor="email">Correo electrónico</label>
-                    <input type="email" id="email" name="email" placeholder="Ingrese su correo electronico" required/>
-              	  </div>
-
-              	  <div className="inputbox">
-                    <label htmlFor="contrasena">Contraseña</label>
-                    <input type="password" id="contrasena" name="contrasena" placeholder="Ingrese su contraseña " required/>
-              	  </div>
-
                 <button type="submit">Registrarse</button>
             </form>
         </div>
